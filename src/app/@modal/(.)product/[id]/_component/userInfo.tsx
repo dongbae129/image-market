ea87ff8552ba527e19bc00d3fd39e2b256a46e22
dongbae129 <dongbae129@naver.com>
2026-07-10 17:21:13 +0900

    feat: modal 댓글유무에 따른 layout shift

--- src/app/@modal/(.)product/[id]/_component/userInfo.tsx
+++ src/app/@modal/(.)product/[id]/_component/userInfo.tsx
@@ -7,11 +7,23 @@ type Props = {
 };
 export default function UserInfo({ productId }: Props) {
   const { data } = useFeedDetail(productId);
-
+  console.log(data, 'userimage');
   return (
     <div className="flex items-center space-x-2">
-      <div className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-200">
-        <User size={16} className="text-gray-500" />
+      <div className="flex overflow-hidden h-8 w-8 items-center justify-center rounded-full bg-gray-200">
+        {data?.product.user.image ? (
+          <img
+            src={data?.product.user.image}
+            alt={`${data?.product.user.id}` || 'userimage'}
+            className="w-full h-full object-cover"
+          />
+        ) : (
+          <User size={16} className="text-gray-500" />
+        )}
+        {/* {data?.product.user.image ?? 
+        <img src={data?.product?.user?.image} alt={data?.product.user.id! || "userimage"} className='w-full h-full object-cover'/> 
+        : 
+        <User size={16} className="text-gray-500" />} */}
       </div>
       <span className="font-bold text-gray-900">
         {data?.product.user.email}

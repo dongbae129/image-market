9768dc3afc9cf75ded04e016ab5565fc96c70529
dongbae129 <dongbae129@naver.com>
2026-09-15 19:23:40 +0900

    feat: board prisma transaction 적용

--- src/app/api/board/route.ts
+++ src/app/api/board/route.ts
@@ -1,12 +1,8 @@
-import { data } from '@/json/price.json';
-// import { dbNow, ResponseType, TokenPayload } from '@libs/server/utils';
-// import { NextApiRequest, NextApiResponse } from 'next';
 import client from '@libs/server/client';
 import { checkAuth } from '@libs/server/auth';
-// // import { decode } from 'jsonwebtoken';
-// import { PostBoardInfo } from './[boardId]';
+
 import { NextRequest, NextResponse } from 'next/server';
-import { dbNow, TokenPayload } from '@libs/server/utils';
+import { dbNow } from '@libs/server/utils';
 import { SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs';
 
 export const GET = async (req: NextRequest) => {
@@ -100,7 +96,6 @@ export const GET = async (req: NextRequest) => {
   }
 };
 export const POST = async (req: NextRequest) => {
-  let feenId = null;
   try {
     const body = await req.json();
 
@@ -131,51 +126,78 @@ export const POST = async (req: NextRequest) => {
             status: 401
           }
         );
-      const sqs = new SQSClient({ region: process.env.AWS_REGION });
       const now = dbNow();
-      const board = await client.board.create({
-        data: {
-          title,
-          description,
-          userId,
-          createdAt: now,
-          updatedAt: now
-        }
-      });
-      feenId = board.id;
-      await Promise.all([
-        client.boardHit.create({
-          data: {
-            hit: 0,
-            boardId: board.id
-          }
-        }),
-        client.boardTag.create({
-          data: {
-            boardId: board.id,
-            hashtag: boardtag
-          }
-        })
-      ]);
-      const boardImageIds = await Promise.all(
-        body.urls.map(async (v: { url: string }) => {
-          const boardImageId = await client.boardImage.create({
+      const result = await client.$transaction(async (tx) => {
+        const board = await tx.board
+          .create({
             data: {
-              boardId: board.id,
-              image: v.url,
-              status: 'PROCESSING'
-            },
-            select: {
-              id: true
+              title,
+              description,
+              userId,
+              createdAt: now,
+              updatedAt: now
             }
+          })
+          .catch((error) => {
+            throw new Error(`BOARD CREATE FAIL | ${error.message}`, {
+              cause: error
+            });
           });
-          return {
-            boardImageId,
-            tempUrl: `temp/${v.url}`
-          };
-        })
-      );
+        await Promise.all([
+          tx.boardHit
+            .create({
+              data: {
+                hit: 0,
+                boardId: board.id
+              }
+            })
+            .catch((error) => {
+              throw new Error(`BOARDHIT FAIL | ${error.message}`, {
+                cause: error
+              });
+            }),
+          tx.boardTag
+            .create({
+              data: {
+                boardId: board.id,
+                hashtag: boardtag
+              }
+            })
+            .catch((error) => {
+              throw new Error(`BOARDTAG FAIL | ${error.message}`, {
+                cause: error
+              });
+            })
+        ]);
 
+        const boardImageIds = await Promise.all(
+          body.urls.map(async (v: { url: string }) => {
+            const boardImageId = await tx.boardImage
+              .create({
+                data: {
+                  boardId: board.id,
+                  image: v.url,
+                  status: 'PROCESSING'
+                },
+                select: {
+                  id: true
+                }
+              })
+              .catch((error) => {
+                throw new Error(`BOARDIMAGE FAIL | ${error.message}`, {
+                  cause: error
+                });
+              });
+            return {
+              boardImageId,
+              tempUrl: `temp/${v.url}`
+            };
+          })
+        );
+        return { board, boardImageIds };
+      });
+      const { board, boardImageIds } = result;
+      const sqs = new SQSClient({ region: process.env.AWS_REGION });
       const s3Payloads = {
         feenId: board.id,
         tempKeys: boardImageIds,
@@ -187,18 +209,6 @@ export const POST = async (req: NextRequest) => {
           MessageBody: JSON.stringify(s3Payloads)
         })
       );
-      // await client.boardHit.create({
-      //   data: {
-      //     hit: 0,
-      //     boardId: board.id
-      //   }
-      // });
-      // await client.boardTag.create({
-      //   data: {
-      //     boardId: board.id,
-      //     hashtag: boardtag
-      //   }
-      // });
       return NextResponse.json({
         ok: true,
         message: 'create the board',
@@ -206,14 +216,14 @@ export const POST = async (req: NextRequest) => {
       });
     }
   } catch (error) {
-    console.log(error, 'board create error');
-    // if (feenId) {
-    //   await client.boar
-    // }
+    if (error instanceof Error) {
+      console.error(error);
+    }
+
     return NextResponse.json(
       {
         ok: false,
-        message: error
+        message: (error as Error).message
       },
       {
         status: 500
@@ -221,121 +231,3 @@ export const POST = async (req: NextRequest) => {
     );
   }
 };
-// const Board = async (
-//   req: NextApiRequest,
-//   res: NextApiResponse<ResponseType>
-// ) => {
-//   if (req.method === 'POST') {
-//     try {
-//       const auth = checkAuth(req, res, 0);
-//       console.log(auth, 'res');
-//       if (auth?.checkError) {
-//         return res.status(401).json({
-//           ok: false,
-//           auth
-//         });
-//       }
-//       if (auth.payload) {
-//         const decoded = auth.payload as TokenPayload;
-//         console.log(decoded, 'decoded');
-//         const { title, description, boardtag }: PostBoardInfo = req.body;
-//         console.log(title, description, boardtag, 'Body');
-//         if (title === '' || description === '')
-//           return res.json({
-//             ok: false,
-//             error: 'input board informations'
-//           });
-//         if (decoded.id) {
-//           const Id = decoded.id;
-//           console.log(Id, 'IID');
-//           const now = dbNow();
-//           const board = await client.board.create({
-//             data: {
-//               title,
-//               description,
-//               userId: Id,
-//               createdAt: now,
-//               updatedAt: now
-//             }
-//           });
-//           console.log(board, 'Boardsd');
-//           await client.boardHit.create({
-//             data: {
-//               hit: 0,
-//               boardId: board.id
-//             }
-//           });
-//           await client.boardTag.create({
-//             data: {
-//               boardId: board.id,
-//               hashtag: boardtag
-//             }
-//           });
-//           return res.json({
-//             ok: true,
-//             message: 'create the board',
-//             board
-//           });
-//         } else
-//           return res.json({
-//             ok: false,
-//             message: 'need to login , /api/board/index, post'
-//           });
-//       }
-//     } catch (error) {
-//       console.log(error, 'board create error');
-//       return res.status(401).json({
-//         ok: false,
-//         message: error
-//       });
-//     }
-//   }
-//   if (req.method === 'GET') {
-//     const searchQuery = req.query.search;
-//     console.log(req.query, 'board query');
-//     try {
-//       const boards = await client.board.findMany({
-//         where: {
-//           ...(searchQuery
-//             ? {
-//                 title: {
-//                   contains: searchQuery.toString()
-//                 }
-//               }
-//             : {})
-//         },
-//         include: {
-//           user: {
-//             select: {
-//               name: true
-//             }
-//           },
-//           boardHit: {
-//             select: {
-//               hit: true
-//             }
-//           },
-//           _count: {
-//             select: {
-//               boardChat: true
-//             }
-//           }
-//         },
-//         orderBy: {
-//           updatedAt: 'desc'
-//         }
-//       });
-//       return res.json({
-//         ok: true,
-//         boards
-//       });
-//     } catch (error) {
-//       console.log(error, 'boards get error');
-//       return res.status(500).json({
-//         ok: false,
-//         message: error
-//       });
-//     }
-//   }
-// };
-// export default Board;
